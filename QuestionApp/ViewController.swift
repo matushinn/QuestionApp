9316bdb702169159f4da749c104a02360d44692a
shotaro-ooe <shoooe97@gmail.com>
2021-07-20 11:43:44 +0900

    firstcommit

---
+++ QuestionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuestionApp
+//
+//  Created by 大江祥太郎 on 2021/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
